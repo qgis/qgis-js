3909ef7cb1b2df58be12a56db5c072b48c73d19b
Michael Schmuki <michael@schmuki.io>
2023-09-22 10:41:35 +0200

    Temporary deployment for @qgis-js/dev (with coi-serviceworker for Github Pages)

--- sites/dev/vite.config.ts
+++ sites/dev/vite.config.ts
@@ -5,7 +5,7 @@ import QgisRuntimePlugin from "../../vite/QgisRuntimePlugin";
 import CrossOriginIsolationPlugin from "../../vite/CrossOriginIsolationPlugin";
 import DirectoryListingPlugin from "../../vite/DirectoryListingPlugin";
 
-import dts from "vite-plugin-dts";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 import packageJson from "./package.json";
 
@@ -36,34 +36,13 @@ export default defineConfig({
     }),
     DirectoryListingPlugin(["public/projects"]),
     CrossOriginIsolationPlugin(),
-    dts({
-      rollupTypes: true,
-      entryRoot: "src",
+    viteStaticCopy({
+      targets: [
+        {
+          src: "node_modules/coi-serviceworker/coi-serviceworker.min.js",
+          dest: "",
+        },
+      ],
     }),
   ],
-  build: {
-    lib: {
-      entry: [resolve(__dirname, "src/index.ts")],
-      name: "qgis-js",
-      formats: ["es"],
-      fileName: "qgis",
-    },
-    rollupOptions: {
-      external: [
-        "vue",
-        "ol",
-        new RegExp("^ol/*"),
-        new RegExp("/lib/demo/.*"),
-        new RegExp("/lib/vite/.*"),
-      ],
-      output: {
-        globals: {
-          vue: "Vue",
-        },
-      },
-    },
-  },
-  server: {
-    open: "/",
-  },
 });
