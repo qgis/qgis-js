fd429fc527a7d1dc1138ff4a80f9f41f372f3d9c
Michael Schmuki <michael@schmuki.io>
2024-02-29 13:05:04 +0100

    Update qca port (based on QField's qca)

--- build/vcpkg-ports/qca/portfile.cmake
+++ build/vcpkg-ports/qca/portfile.cmake
@@ -11,15 +11,15 @@ vcpkg_add_to_path("${PERL_EXE_PATH}")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KDE/qca
-    REF v2.3.6
-    SHA512 983cd023482a2c16e4976a8b75ff739239f82aab039dc879c0cbfb842487d77fb4b153bf3c855bed0ec6b1a48f2c4f654c181982a945e2f83cb9953c4896c70d
+    REF "v${VERSION}"
+    SHA512 de06173aaea32aac19a24510b5dbb4bb79681217eb1e4256de36db9f7158ad485fa450ffba5e13c12a0425866923b54f9b4d6164d0eaf659fdf40e458f5ee017
     PATCHES
         0001-fix-path-for-vcpkg.patch
-#        0002-fix-build-error.patch
-#        0003-Define-NOMINMAX-for-botan-plugin-with-MSVC.patch
-#        ios.patch
-#        connect_fix.patch
+        0002-fix-build-error.patch
+        0003-Define-NOMINMAX-for-botan-plugin-with-MSVC.patch
 )
+
+vcpkg_find_acquire_program(PKGCONFIG)
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
   set(QCA_FEATURE_INSTALL_DIR_DEBUG ${CURRENT_PACKAGES_DIR}/debug/bin/Qca)
@@ -35,6 +35,7 @@ endif()
 # So we do it here:
 message(STATUS "Importing certstore")
 file(REMOVE "${SOURCE_PATH}/certs/rootcerts.pem")
+
 # Using file(DOWNLOAD) to use https
 file(DOWNLOAD https://raw.githubusercontent.com/mozilla/gecko-dev/master/security/nss/lib/ckfw/builtins/certdata.txt
     "${CURRENT_BUILDTREES_DIR}/cert/certdata.txt"
@@ -47,26 +48,30 @@ vcpkg_execute_required_process(
 )
 message(STATUS "Importing certstore done")
 
-if(VCPKG_CROSSCOMPILING)
-   list(APPEND QCA_OPTIONS -DQT_HOST_PATH=${CURRENT_HOST_INSTALLED_DIR})
-   list(APPEND QCA_OPTIONS -DQT_ADDITIONAL_PACKAGES_PREFIX_PATH=${CURRENT_HOST_INSTALLED_DIR})
-   list(APPEND QCA_OPTIONS -DQT_HOST_PATH_CMAKE_DIR:PATH=${CURRENT_HOST_INSTALLED_DIR}/share)
+set(PLUGINS logger softstore wincrypto)
+if("botan" IN_LIST FEATURES)
+    list(APPEND PLUGINS botan)
 endif()
- 
-list(APPEND QCA_OPTIONS -DWITH_gnupg_PLUGIN=no)
+if ("gnupg" IN_LIST FEATURES)
+    list(APPEND PLUGINS gnupg)
+endif()
+if ("ossl" IN_LIST FEATURES)
+    list(APPEND PLUGINS ossl)
+endif()
+
 # Configure and build
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DBUILD_WITH_QT6=ON
         -DUSE_RELATIVE_PATHS=ON
+        "-DBUILD_PLUGINS=${PLUGINS}"
         -DBUILD_TESTS=OFF
         -DBUILD_TOOLS=OFF
+        -DBUILD_WITH_QT6=ON
         -DQCA_SUFFIX=OFF
         -DQCA_FEATURE_INSTALL_DIR=share/qca/mkspecs/features
         -DOSX_FRAMEWORK=OFF
-        -DQT6=ON
-        ${QCA_OPTIONS}
+        "-DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}"
     OPTIONS_DEBUG
         -DQCA_PLUGINS_INSTALL_DIR=${QCA_FEATURE_INSTALL_DIR_DEBUG}
     OPTIONS_RELEASE
@@ -91,5 +96,7 @@ file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/share"
 )
 
+vcpkg_fixup_pkgconfig()
+
 # Handle copyright
-file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
