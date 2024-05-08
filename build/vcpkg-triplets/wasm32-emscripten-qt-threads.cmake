44da15774113b145cb0ec27ff3f8fae35a742bf1
Michael Schmuki <michael@schmuki.io>
2024-05-08 10:10:27 +0200

    Cleanup of CMake related files

--- build/vcpkg-triplets/wasm32-emscripten-qt-threads.cmake
+++ build/vcpkg-triplets/wasm32-emscripten-qt-threads.cmake
@@ -5,8 +5,8 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Emscripten)
 
-# to avoid building both debug and release of all libs
-set(VCPKG_BUILD_TYPE "release")
+# to avoid building both debug and release of all libs uncomment the following line
+# set(VCPKG_BUILD_TYPE "release")
 
 set(VCPKG_ENV_PASSTHROUGH_UNTRACKED EMSDK EMSCRIPTEN EMSCRIPTEN_ROOT PATH)
 
@@ -19,6 +19,3 @@ set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE ${QGISJS_TOOLCHAIN_FILE})
 
 set(VCPKG_ENV_PASSTHROUGH_UNTRACKED EMSDK EMSCRIPTEN EMSCRIPTEN_ROOT PATH)
 
-# set(VCPKG_ENV_PASSTHROUGH Qt6_DIR) # needed by port/qt6/vcpkg-cmake-wrapper.cmake
-# set(VCPKG_ENV_PASSTHROUGH QT_HOST_PATH) # needed by port/qt6/vcpkg-cmake-wrapper.cmake
-# set(QT_HOST_PATH $ENV{QT_HOST_PATH} CACHE PATH "needed by QtPublicDependencyHelpers.cmake" FORCE)
