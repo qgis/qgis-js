e767ee012176e5546e08babd6f7a6ccdfb30fa7f
Michael Schmuki <michael@schmuki.io>
2026-03-13 22:21:02 +0100

    Update emsdk to 5.0.2

--- build/scripts/install.sh
+++ build/scripts/install.sh
@@ -2,8 +2,8 @@
 set -eo pipefail
 
 git submodule update --init build/emsdk
-build/emsdk/emsdk install 4.0.22;
-build/emsdk/emsdk activate 4.0.22;
+build/emsdk/emsdk install 5.0.2;
+build/emsdk/emsdk activate 5.0.2;
 
 git submodule update --init build/vcpkg
 ./build/vcpkg/bootstrap-vcpkg.sh -disableMetrics
