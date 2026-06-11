b2d849bc836357422b320fc8cc9bf4d6435e9d7b
Michael Schmuki <michael@schmuki.io>
2026-06-11 23:48:19 +0200

    chore(engines): update emsdk 5.0.2 -> 5.0.7

--- build/scripts/install.sh
+++ build/scripts/install.sh
@@ -2,8 +2,8 @@
 set -eo pipefail
 
 git submodule update --init build/emsdk
-build/emsdk/emsdk install 5.0.2;
-build/emsdk/emsdk activate 5.0.2;
+build/emsdk/emsdk install 5.0.7;
+build/emsdk/emsdk activate 5.0.7;
 
 git submodule update --init build/vcpkg
 ./build/vcpkg/bootstrap-vcpkg.sh -disableMetrics
